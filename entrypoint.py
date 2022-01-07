dc5ce04f75d3ee4208f01ba3c9f5b7cabdc8f80d
Daniel Saunders <daniel.saunders@mail.utoronto.ca>
2022-01-07 17:26:50 -0500

    Procfile

--- entrypoint.py
+++ entrypoint.py
@@ -2,4 +2,4 @@ import uvicorn
 from app.settings import settings
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=int(settings.PORT), reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(settings.PORT))
